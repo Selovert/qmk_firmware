c2ad1584371b830f26193bbf58eda1284bdad1fb
Tassilo Selover-Stephan <tassilo@selover-stephan.com>
2025-11-12 14:48:12 -0800

    update to new QMK format. switch to win11 shortcut for muting

--- keyboards/selovert/croaky4/keymaps/default/keymap.c
+++ keyboards/selovert/croaky4/keymaps/default/keymap.c
@@ -1,24 +1,7 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/* Copyright 2020 Selovert
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
- * qmk compile -kb selovert/croaky4 -km default
-
- */
 #include QMK_KEYBOARD_H
-#include "print.h"
 
 bool micMuted = 0; // Track mic mute state
 #define n_led 7 // number of LEDs to toggle while muting
@@ -46,31 +29,37 @@ enum td_keycodes {
 };
 
 // Function to determine the current tapdance state
-td_state_t cur_dance(qk_tap_dance_state_t *state);
+td_state_t cur_dance(tap_dance_state_t *state);
 
 // `finished` and `reset` functions for each tapdance keycode
-void teams_mt_finished(qk_tap_dance_state_t *state, void *user_data);
-void teams_mt_reset(qk_tap_dance_state_t *state, void *user_data);
+void teams_mt_finished(tap_dance_state_t *state, void *user_data);
+void teams_mt_reset(tap_dance_state_t *state, void *user_data);
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-                     KC_MPRV       , KC_MNXT ,
+    /*
+     * ┌───┬───┬───┬───┬───┐
+     * │ - │ - │ 1 │ - │ 2 │
+     * ├───┼───┼───┼───┼───┤
+     * │ E │ - │ - │ - │ - │
+     * ├───┼───┼───┼───┼───┤
+     * │ - │ - │ 3 │ - │ 4 │
+     * └───┴───┴───┴───┴───┘
+     */
+    [0] = LAYOUT_4x4_w_encoder(
+                        KC_MPRV,        KC_MNXT,
         LT(1,KC_MUTE),
-                     TD(TEAMS_MT)  , KC_MPLY
+                        TD(TEAMS_MT),   KC_MPLY
     ),
-
-    [1] = LAYOUT(
-                     RESET   , _______ ,
-        _______ ,
-                     _______ , _______
+    [1] = LAYOUT_4x4_w_encoder(
+                QK_BOOT,    _______,
+        _______,
+                _______,    _______
     )
 };
 
-void keyboard_post_init_user(void) {
-    debug_enable=true;
-}
- // Encoder functions
+// Encoder functions
+#ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {
     switch(biton32(layer_state)){ // check layer state
         case 0:
@@ -90,6 +79,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     }
     return true;
 }
+#endif
 
 // Toggle state of LEDs in array
 void toggle_led(int leds[], bool mute_state) {
@@ -107,18 +97,16 @@ void toggle_led(int leds[], bool mute_state) {
 void teams_mute(bool mute_state) {
     if (mute_state) {
         toggle_led(target_leds, mute_state);
-        print("Muting");
-        tap_code16(C(KC_F13));
+        tap_code16(G(A(KC_K)));
     }
     else {
         toggle_led(target_leds, mute_state);
-        print("Unmuting");
-        tap_code16(C(KC_F13));
+        tap_code16(G(A(KC_K)));
     }
 }
 
 // Determine the tapdance state to return
-td_state_t cur_dance(qk_tap_dance_state_t *state) {
+td_state_t cur_dance(tap_dance_state_t *state) {
     if (state->count == 1) {
         if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
         else return TD_SINGLE_HOLD;
@@ -137,7 +125,7 @@ static td_tap_t td_state = {
 };
 
 // Trigger when tapdance finishes (fires at beginning of a hold)
-void teams_mt_finished(qk_tap_dance_state_t *state, void *user_data) {
+void teams_mt_finished(tap_dance_state_t *state, void *user_data) {
     td_state.state = cur_dance(state); // pull number of clicks and hold state
     switch (td_state.state) {
         case TD_SINGLE_TAP:
@@ -161,7 +149,7 @@ void teams_mt_finished(qk_tap_dance_state_t *state, void *user_data) {
 }
 
 // Trigger when tapdance resets (fires at end of a hold)
-void teams_mt_reset(qk_tap_dance_state_t *state, void *user_data) {
+void teams_mt_reset(tap_dance_state_t *state, void *user_data) {
     switch (td_state.state) {
         case TD_SINGLE_TAP:
             break;
@@ -179,6 +167,6 @@ void teams_mt_reset(qk_tap_dance_state_t *state, void *user_data) {
 }
 
 // Define `ACTION_TAP_DANCE_FN_ADVANCED()` for each tapdance keycode, passing in `finished` and `reset` functions
-qk_tap_dance_action_t tap_dance_actions[] = {
+tap_dance_action_t tap_dance_actions[] = {
     [TEAMS_MT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, teams_mt_finished, teams_mt_reset)
 };
--- keyboards/selovert/croaky4/readme.md
+++ keyboards/selovert/croaky4/readme.md
@@ -1,15 +1,27 @@
-# Planck
+# selovert/croaky4
 
-![Croaky4](https://i.imgur.com/q2M3uEU.jpg)
+![selovert/croaky4](imgur.com image replace me!)
 
-A compact 40% (12x4) ortholinear keyboard kit made and sold by OLKB and Massdrop. [More info on qmk.fm](https://qmk.fm/planck/)
+*A short description of the keyboard/project*
 
-* Keyboard Maintainer: [Selovert](https://github.com/selovert)
-* Hardware Supported: [Croaky4](https://github.com/Selovert/Croaky4)
-* Hardware Availability: [Self build from source](https://github.com/Selovert/Croaky4)
+* Keyboard Maintainer: [selovert](https://github.com/selovert)
+* Hardware Supported: *The PCBs, controllers supported*
+* Hardware Availability: *Links to where you can find this hardware*
 
 Make example for this keyboard (after setting up your build environment):
 
     make selovert/croaky4:default
 
+Flashing example for this keyboard:
+
+    make selovert/croaky4:default:flash
+
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
+
+## Bootloader
+
+Enter the bootloader in 3 ways:
+
+* **Bootmagic reset**: Hold down the key at (0,0) in the matrix (usually the top left key or Escape) and plug in the keyboard
+* **Physical reset button**: Briefly press the button on the back of the PCB - some may have pads you must short instead
+* **Keycode in layout**: Press the key mapped to `QK_BOOT` if it is available
